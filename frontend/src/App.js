dd14bb9931d4e5d6e86ad182abf63f606aa610d1
felipealvss <lipealvessax@gmail.com>
2019-06-13 16:54:47 -0300

    Feed layout implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './global.css';
 
+import Header from './components/Header';
 import Routes from './routes';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
 
     <BrowserRouter>
+      <Header />
       <Routes />
     </BrowserRouter>
   )
--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,5 +1,11 @@
 
 import React, { Component } from 'react';
+import './Feed.css';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
 
 class Feed extends Component {
 
@@ -7,7 +13,43 @@ class Feed extends Component {
 
         return(
             
-            <h1>Feed</h1>
+            <section id="post-list">
+
+                <article>
+
+                    <header>
+
+                        <div className="user-info">
+
+                            <span>Felipe Alves</span>
+                            <span className="plae">Rio do Sul</span>
+                        </div>
+
+                        <img src={more} alt="Mais" />
+                    </header>
+
+                    <img src="http://localhost:3030/files/wallpaper.jpg" alt="" />
+
+                    <footer>
+
+                          <div className="actions">
+
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />
+                          </div>
+
+                          <strong>837 curtidas</strong>
+
+                          <p>
+
+                            Post só o filé aqui, pense!
+                            <span>#SóOShow #VaiDarBom #Correria #CursoJS</span>
+                          </p>
+
+                    </footer>
+                </article>
+            </section>
         )
     }
 }
